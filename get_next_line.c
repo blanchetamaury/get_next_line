4b231563908dc51b5d8ea5d2e769bdd3fc4890dd
Amaury Blanchet <amblanch@2C2.42angouleme.fr>
2024-10-30 08:40:08 +0100

    gnl

--- get_next_line.c
+++ get_next_line.c
@@ -1,41 +1,17 @@
-#include <fcntl.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amblanch <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/29 13:03:58 by amblanch          #+#    #+#             */
+/*   Updated: 2024/10/29 13:04:02 by amblanch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-void  ft_print(char *buf)
-{
-  int j;
-  
-  j = 0;
-  while(buf[j])
-  {
-    write(1, &buf[j], 1);
-    j++;
-  }
-}
-size_t	ft_strlen(const char *s)
-{
-	int	i;
+#include "get_next_line.h"
 
-	i = 0;
-	while (s[i])
-		i++;
-	return (i);
-}
-int ft_strchr(const char *s, int c)
-{
-	size_t	i;
-
-	i = 0;
-	while (s[i])
-	{
-		if (s[i] == (unsigned char)c)
-			return (i + 1);
-		i++;
-	}
-	return (0);
-}
 char	*ft_memset(char *s, int c, size_t n)
 {
 	size_t	i;
@@ -48,84 +24,102 @@ char	*ft_memset(char *s, int c, size_t n)
 	}
 	return (s);
 }
-char  *ft_stoker(char *buf, int len, char *line)
-{
-  int i;
-  int j;
-  
-  i = 0;
-  j = 0;
-  while (line[i])
-    i++;
-  while (j < len && buf[j])
-  {
-    line[i + j] = buf[j];
-    j++;
-  }
-  line[i + j] = '\0';
-  return (line);
-}
-char  *get_next_line(int fd)
-{
-  char buf[BUFFER_SIZE];
-  char *line;
-  static char res[BUFFER_SIZE];
-  int  len;
-  int         bytesread;
-  
-  //printf("res = [%s]", res);
-  //printf("buf = [%s]", buf);
-  line = malloc(sizeof(char) * 1024);
-  if (line == 0)
-    return (0);
-  ft_memset(line, 0, BUFFER_SIZE + 1);
-  if (ft_strlen(res) > 0)
-  {
-    len = ft_strchr(res, '\n');
-     //printf("res = [%d]", len);
-    if (len > 0)
-    {
-        line = ft_stoker(res, len, line);
-        //ft_stoker(&res[len] + 1, ft_strlen(&res[len] - 1), res);
-        bytesread = 0;
-        while (res[len + bytesread] != '\n' && res[len + bytesread] != '\0')
-        {
-          res[bytesread] = res[len + bytesread];
-          bytesread++;
-        }
-        res[bytesread] = '\n';
-        //printf("res = [%s]", res);
-        ft_print(line);
-        return (line);
-    } 
-    else 
-    {
-        ft_stoker(res, ft_strlen(res), line);
-    }
-  }
- while ((bytesread = read(fd, buf, BUFFER_SIZE)) > 0) {
-        if (bytesread == -1)
-        {
-          free(line);
-          return (0);
-        }
-        //buf[bytesread] = '\0'; // Null-terminate le buffer
-        len = ft_strchr(buf, '\n');
-        if (len > 0) {
-            ft_stoker(buf, len, line);
-            ft_memset(res, 0, BUFFER_SIZE);
-            ft_stoker(&buf[len], bytesread - len, res);
-            ft_print(line);
-            return (line);
-        } else {
-            ft_stoker(buf, bytesread, line);
-        }
-    }
 
-    if (ft_strlen(line) > 0)
-    {
-        ft_print(line);
-        return (line);
-    }
-    return (NULL);
+size_t	ft_strlcpy(char *dst, char *src, size_t size)
+{
+	size_t	i;
+
+	i = 0;
+	if (dst == 0 || src == 0)
+		return (0);
+	if (size)
+	{
+		while (*src && i < size)
+		{
+			*dst++ = *src++;
+			i++;
+		}
+		*dst = '\0';
+	}
+	return (ft_strlen(src) + i);
+}
+
+char	*ft_res(char *line, char *buf, int bytesread, int i)
+{
+	static char	res[BUFFER_SIZE];
+	int			len;
+
+	while (ft_strlen(res) > 0)
+	{
+		len = ft_strchr(res, '\n');
+		if (len > 0)
+		{
+			line = ft_strjoin(line, res, len);
+			ft_strlcpy(res, &res[len], BUFFER_SIZE - len);
+			free(buf);
+			return (line);
+		}
+		else
+		{
+			line = ft_strjoin(line, res, BUFFER_SIZE);
+			ft_memset(res, 0, BUFFER_SIZE);
+		}
+	}
+	if (bytesread > 0)
+		ft_strlcpy(res, &buf[i], bytesread);
+	return (line);
+}
+
+char	*ft_line(char *buf, char *line, int fd)
+{
+	int	bytesread;
+	int	len;
+
+	bytesread = read(fd, buf, BUFFER_SIZE);
+	while (bytesread > 0)
+	{
+		buf[bytesread] = '\0';
+		len = ft_strchr(buf, '\n');
+		if (len >= 0)
+		{
+			line = ft_strjoin(line, buf, len);
+			ft_res(line, buf, BUFFER_SIZE - len, len);
+			free(buf);
+			return (line);
+		}
+		else
+			line = ft_strjoin(line, buf, BUFFER_SIZE);
+		bytesread = read(fd, buf, BUFFER_SIZE);
+	}
+	if (bytesread == -1)
+	{
+		free(line);
+		return (NULL);
+	}
+	return (line);
+}
+
+char	*get_next_line(int fd)
+{
+	char		*buf;
+	char		*line;
+
+	if (fd == -1)
+		return (0);
+	buf = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
+	line = ft_calloc(BUFFER_SIZE, sizeof(char));
+	line = ft_memset(line, 0, BUFFER_SIZE);
+	if (line == 0)
+		return (0);
+	line = ft_res(line, buf, 0, 0);
+	if (ft_strchr(line, '\n') > 0)
+		return (line);
+	line = ft_line(buf, line, fd);
+	if (ft_strchr(line, '\n') > 0)
+		return (line);
+	free(buf);
+	if (ft_strlen(line) > 0)
+		return (line);
+	free(line);
+	return (NULL);
 }
